1af077e4a8ccb9d23595b32fbdc5984685aed720
Ken Stone <ken@stoneblue.com>
2020-04-16 16:04:00 -0500

    Nested elements

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-// We also need "Component" so that we can
-// extend it and make a new JSX tag.
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-// "MyComponent" extends "Compoennt", which means that
-// we can now use it in JSX markup.
-class MyComponent extends Component {
-  render() {
-    // All components have a "render()" method, which
-    // retunrns some JSX markup. In this case, "MyComponent"
-    // encapsulates a larger HTML structure.
-    return (
-      <section>
-        <h1>My Component</h1>
-        <p>Content in my component...</p>
-      </section>
-    );
-  }
-}
+// Imports our two components that render children...
+import MySection from './MySection';
+import MyButton from './MyButton';
 
-// Now when we render "<MyComponent>" tags, the encapsulated
-// HTML structure is actually rendered. These are the
-// building blocks of our UI.
-render(<MyComponent />, document.getElementById('root'));
+// Renders the "MySection" element, which has a child
+// component of "MyButton", which in turn has child text.
+render(
+  <MySection>
+    <MyButton>My Button Text</MyButton>
+  </MySection>,
+  document.getElementById('root')
+);
