1a323cb890356548160ebad612f52fc25493dc14
Ken Stone <ken@stoneblue.com>
2020-04-18 16:08:21 -0500

    Built-in HTML tags

---
+++ src/RenderingJSX/built-in-html-tags.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class BuiltInHtmlTags extends React.Component {
+
+  render() {
+    return(
+
+      <section>
+        <div>
+          <button />
+          <code />
+          <input />
+          <label />
+          <p />
+          <pre />
+          <select />
+          <table />
+          <ul />
+        </div>
+      </section>
+
+    );
+  }
+}
+
+export default BuiltInHtmlTags;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import MappingCollectionsToElements from './RenderingJSX/mapping-collections-to-elements.js';
+import Chance from 'chance';
 
+import MappingCollectionsToElements from './RenderingJSX/mapping-collections-to-elements.js';
+import BuiltInHtmlTags from './RenderingJSX/built-in-html-tags.js';
+
+var chance = new Chance();
 const enabled = true;
 const text = 'Y Button';
 const placeholder = 'two inputs big value...';
@@ -17,7 +21,9 @@ const size = 90;
 
 render(
   <section>
+    <p>Hello, <strong>{chance.first()}</strong> from {chance.country({ full: true })} listening on {chance.radio()}</p>
     <MappingCollectionsToElements  />
+    <BuiltInHtmlTags  />
     <button disabled={!enabled}>{text}</button>
     <input placeholder={placeholder} size={size} />
   </section>,
