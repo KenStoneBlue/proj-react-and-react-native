b50d6821e7eca5c3ea8ad4a58af5dd4732ff05a8
Ken Stone <ken@stoneblue.com>
2020-04-18 12:55:29 -0500

    Mapping collections to elements

---
+++ src/RenderingJSX/mapping-collections-to-elements.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const array = ['First', 'Second', 'Third'];
+const object = {
+  first: 1,
+  second: 2,
+  third: 3
+};
+
+
+class MappingCollectionsToElements extends React.Component {
+
+  render() {
+    return(
+
+      <section>
+        <h1>Mapping Array</h1>
+        {/* Maps "array" to an array of "<li>"s.
+             Note the "key" property on "<li>".
+             This is necessary for performance reasons,
+             and React will warn us if it's missing. */}
+        <ul>{array.map(i => <li key={i}>{i}</li>)}</ul>
+  
+  
+        <h1>Mapping Object</h1>
+        {/* Maps "object" to an array of "<li>"s.
+             Note that we have to use "Object.keys()"
+             before calling "map()" and that we have
+             to lookup the value using the key "i". */}
+        <ul>
+          {Object.keys(object).map(i => (
+            <li key={i}>
+              <strong>{i}: </strong>
+              {object[i]}
+            </li>
+          ))}
+        </ul>
+  
+      </section>
+
+    );
+  }
+}
+
+export default MappingCollectionsToElements;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// These constants are passed into the JSX
-// markup using the JavaScript expression syntax.
+import MappingCollectionsToElements from './RenderingJSX/mapping-collections-to-elements.js';
+
 const enabled = true;
 const text = 'Y Button';
 const placeholder = 'two inputs big value...';
 const size = 90;
 
-// We're rendering a "<button>" and an "<input>"
-// element, both of which use the "{}" JavaScript
-// expression syntax to fill in property, and text
-// values.
+
+
+
+
+
+
+
 render(
   <section>
+    <MappingCollectionsToElements  />
     <button disabled={!enabled}>{text}</button>
     <input placeholder={placeholder} size={size} />
   </section>,
